aabf3ae31706dec3c9997632eeb6304e95c75dd9
98001yash <yashchauhan.gaya@gmail.com>
2026-01-10 19:09:34 +0530

    implemented the JwtService

---
+++ src/main/java/com/company/tradius_backend/security/JwtService.java
@@ -0,0 +1,51 @@
+package com.company.tradius_backend.security;
+
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+
+    public String generateToken(String userId, String email, String role) {
+        return Jwts.builder()
+                .subject(userId)
+                .claims(Map.of(
+                        "email", email,
+                        "role", role
+                ))
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
