3aa3c95102a3557a49286d0afb21e96aec36ed3c
98001yash <yashchauhan.gaya@gmail.com>
2026-01-10 17:22:59 +0530

    added the application.propertied and written the configuration file

---
+++ src/main/java/com/company/tradius_backend/TradiusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.tradius_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradiusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradiusBackendApplication.class, args);
+	}
+
+}
