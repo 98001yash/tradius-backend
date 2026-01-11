faae2c173432333c69bfbd879cf9b037675c33a2
98001yash <yashchauhan.gaya@gmail.com>
2026-01-11 13:01:26 +0530

    implemented the user-profile entity class

--- src/main/java/com/company/tradius_backend/entities/UserProfile.java
+++ src/main/java/com/company/tradius_backend/entities/UserProfile.java
@@ -1,4 +1,56 @@
 package com.company.tradius_backend.entities;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "user_profiles")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            unique = true
+    )
+    private User user;
+
+    @Column(length = 100)
+    private String firstName;
+
+    @Column(length = 100)
+    private String lastName;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    private String profileImageUrl;
+
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = OffsetDateTime.now();
+        this.updatedAt = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = OffsetDateTime.now();
+    }
 }
