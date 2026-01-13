ce27cd960a245ea8449427aadab7bba1a84b7a0c
98001yash <yashchauhan.gaya@gmail.com>
2026-01-13 17:28:23 +0530

    added the booking service interface and defined some API methods

---
+++ src/main/java/com/company/tradius_backend/service/BookingService.java
@@ -0,0 +1,38 @@
+package com.company.tradius_backend.service;
+
+import com.company.tradius_backend.dtos.BookingResponseDto;
+import com.company.tradius_backend.dtos.CreateBookingRequestDto;
+import com.company.tradius_backend.enums.BookingStatus;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BookingService {
+
+    // USER -> create a booking request
+    BookingResponseDto createBooking(CreateBookingRequestDto request);
+
+
+    // USER-> view own bookings
+    List<BookingResponseDto> getMyBookings(UUID serviceId);
+
+
+    // USER-> cancel bookings (only if allowed)
+    BookingResponseDto cancelBooking(UUID bookingId);
+
+    // VENDOR-> view bookings for own services
+    List<BookingResponseDto> getVendorBookings();
+
+    // VENDOR-? Filter bookings by status
+    List<BookingResponseDto> getVendorBookingsByStatus(BookingStatus status);
+
+    // VENDOR-> accept bookings
+    BookingResponseDto acceptBooking(UUID bookingId);
+
+
+    // VENDOR-> Reject bookings
+    BookingResponseDto rejectBooking(UUID bookingId);
+
+    // VENDOR -> Mark booking as completed
+    BookingResponseDto completeBooking(UUID bookingId);
+}
