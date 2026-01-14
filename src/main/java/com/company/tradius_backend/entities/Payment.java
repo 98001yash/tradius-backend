0e70a621b1877657700a622e85610bbf4eba538f
98001yash <yashchauhan.gaya@gmail.com>
2026-01-14 08:08:43 +0530

    added the Payment entity

---
+++ src/main/java/com/company/tradius_backend/entities/Payment.java
@@ -0,0 +1,59 @@
+package com.company.tradius_backend.entities;
+
+
+import com.company.tradius_backend.enums.PaymentMethod;
+import com.company.tradius_backend.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "booking_id", nullable = false, unique = true)
+    private Booking booking;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod method;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    // razorPay specific fields
+    private String razopayOrderId;
+    private String razorpayPaymentId;
+    private String razorpaySignature;
+
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.status = PaymentStatus.CREATED;
+        this.createdAt = OffsetDateTime.now();
+        this.updatedAt = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = OffsetDateTime.now();
+    }
+}
