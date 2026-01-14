f3e2cc24b2fa46e17d515f53d759927e2ef210bf
98001yash <yashchauhan.gaya@gmail.com>
2026-01-14 08:30:39 +0530

    implemented the paymentService interface

---
+++ src/main/java/com/company/tradius_backend/service/Impl/PaymentServiceImpl.java
@@ -0,0 +1,83 @@
+package com.company.tradius_backend.service.Impl;
+
+
+import com.company.tradius_backend.dtos.CreatePaymentResponseDto;
+import com.company.tradius_backend.entities.Booking;
+import com.company.tradius_backend.entities.Payment;
+import com.company.tradius_backend.enums.BookingStatus;
+import com.company.tradius_backend.enums.PaymentMethod;
+import com.company.tradius_backend.enums.PaymentStatus;
+import com.company.tradius_backend.exceptions.ResourceNotFoundException;
+import com.company.tradius_backend.exceptions.RuntimeConflictException;
+import com.company.tradius_backend.repository.BookingRepository;
+import com.company.tradius_backend.repository.PaymentRepository;
+import com.company.tradius_backend.service.PaymentService;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.json.JSONObject;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PaymentServiceImpl implements PaymentService {
+
+    private final RazorpayClient razorpayClient;
+    private final BookingRepository bookingRepository;
+    private final PaymentRepository paymentRepository;
+
+    @Override
+    @PreAuthorize("hasRole('USER')")
+    public CreatePaymentResponseDto createRazorpayOrder(UUID bookingId) throws RazorpayException {
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new ResourceNotFoundException("Booking not found"));
+
+        if (booking.getStatus() != BookingStatus.ACCEPTED) {
+            throw new RuntimeConflictException(
+                    "Payment can be initiated only after vendor accepts the booking"
+            );
+        }
+        paymentRepository.findByBooking_Id(bookingId)
+                .ifPresent(p -> {
+                    throw new RuntimeConflictException("Payment already initiated");
+                });
+
+        long amountInPaise = (long) (booking.getServiceOffering().getPrice() * 100);
+
+        JSONObject orderRequest = new JSONObject();
+        orderRequest.put("amount", amountInPaise);
+        orderRequest.put("currency", "INR");
+        orderRequest.put("receipt", booking.getId().toString());
+
+        Order razorpayOrder = razorpayClient.orders.create(orderRequest);
+
+        Payment payment = Payment.builder()
+                .booking(booking)
+                .amount(amountInPaise)
+                .method(PaymentMethod.RAZORPAY)
+                .status(PaymentStatus.ORDER_CREATED)
+                .razopayOrderId(razorpayOrder.get("id"))
+                .build();
+
+
+        paymentRepository.save(payment);
+
+        log.info("Razorpay order {} created for booking {}",
+                razorpayOrder.get("id"), bookingId);
+
+        return new CreatePaymentResponseDto(
+                razorpayOrder.get("id"),
+                amountInPaise,
+                "INR"
+        );
+    }
+}
--- src/main/java/com/company/tradius_backend/service/PaymentService.java
+++ src/main/java/com/company/tradius_backend/service/PaymentService.java
@@ -1,10 +1,11 @@
 package com.company.tradius_backend.service;
 
 import com.company.tradius_backend.dtos.CreatePaymentResponseDto;
+import com.razorpay.RazorpayException;
 
 import java.util.UUID;
 
 public interface PaymentService {
 
-    CreatePaymentResponseDto createRazorpayOrder(UUID bookingId);
+    CreatePaymentResponseDto createRazorpayOrder(UUID bookingId) throws RazorpayException;
 }
