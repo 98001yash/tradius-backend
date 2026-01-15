f2fd338c47172afcc88586ae79ca4f98c53e2123
98001yash <yashchauhan.gaya@gmail.com>
2026-01-15 17:56:07 +0530

    implemented the razorWrbHookController

--- src/main/java/com/company/tradius_backend/controller/RazorpayWebhookController.java
+++ src/main/java/com/company/tradius_backend/controller/RazorpayWebhookController.java
@@ -1,14 +1,27 @@
 package com.company.tradius_backend.controller;
 
 
+import com.company.tradius_backend.service.RazorpayWebhookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/webhooks")
 @RequiredArgsConstructor
 @Slf4j
 public class RazorpayWebhookController {
+
+    private final RazorpayWebhookService webhookService;
+
+    @PostMapping("/razorpay")
+    public ResponseEntity<Void> handleRazorpayWebhook(
+            @RequestBody String payload,
+            @RequestHeader("X-Razorpay-Signature") String signature
+    ) {
+        log.info("Razorpay webhook received");
+        webhookService.processWebhook(payload, signature);
+        return ResponseEntity.ok().build(); // ALWAYS 200
+    }
 }
